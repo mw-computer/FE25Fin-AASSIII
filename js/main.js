f15b298386a315edcb3e096592495f2e8a543979
이세은 <silver9256@naver.com>
2025-06-21 18:32:25 +0900

    두 번째 커밋

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-//TODO: 동적 기능 구현
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('fixed', window.scrollY > 50);
+});
+
+// 맨 위로가기 버튼
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+  topBtn.style.display = window.scrollY > 200 ? "block" : "none";
+});
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
